738e5b180d14c40c970ce6969aecead92df27128
Stephen Fulljames <stephen@fulljames.net>
2012-04-27 07:39:16 +0200

    Switched to built-in express LESS compiler. Removed dependency on step.

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var express = require('express');
+var express = require('express'),
+    public = __dirname + '/public';
 
 var controllers = require('./controllers');
 
-var app = module.exports = express.createServer(
-    express.static(__dirname + '/public')
-);
+var app = module.exports = express.createServer();
 
-app.configure(function () {
+app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.compiler({ src: public, enable: ['less']}));
+    app.use(express.static(public));
 });
 
 app.get('/',controllers.home.index);
@@ -17,9 +21,6 @@ app.get('/s/:term',controllers.home.index);
 
 app.post('/',controllers.home.index);
 
-
 // 17178
 app.listen(process.argv.length > 2 ? parseInt(process.argv[2]) : 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-
-require('./modules/compileCss');
